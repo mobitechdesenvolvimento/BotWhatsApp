13d0bbd4b10b1766e1887f9ca433738d5c9b7ade
mobitechdesenvolvimento <mobitechdesenvolvimento@gmail.com>
2024-08-08 12:39:31 -0300

    commit

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ logging.basicConfig(filename='whatsapp_automation.log', level=logging.INFO,
 stop_event = Event()
 logs = {"success": [], "failure": []}
 
+
 def criar_modelo_planilha():
     # Cria um modelo de planilha com colunas "Nome" e "Telefone"
     workbook = openpyxl.Workbook()
@@ -25,24 +26,29 @@ def criar_modelo_planilha():
     sheet.title = "Modelo"
     sheet.append(["Nome", "Telefone"])
     # Adiciona uma linha de exemplo
-    sheet.append(["Exemplo Nome", "5513997279402"])  # Número de exemplo com código do país (Brasil)
+    # Número de exemplo com código do país (Brasil)
+    sheet.append(["Exemplo Nome", "5513997279402"])
     return workbook
+
 
 def baixar_modelo():
     # Função para salvar o modelo de planilha no local especificado pelo usuário
     modelo_workbook = criar_modelo_planilha()
-    save_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")])
+    save_path = filedialog.asksaveasfilename(
+        defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")])
     if not save_path:
         return
-    
+
     modelo_workbook.save(save_path)
     messagebox.showinfo("Sucesso", "Modelo de planilha baixado com sucesso!")
+
 
 def enviar_mensagens(file_path, wait_time_whatsapp, wait_time_message, mensagem_template):
     try:
         # Abrir WhatsApp Web
         webbrowser.open('https://web.whatsapp.com/')
-        time.sleep(wait_time_whatsapp)  # Aguarda para que o WhatsApp Web carregue
+        # Aguarda para que o WhatsApp Web carregue
+        time.sleep(wait_time_whatsapp)
 
         # Ler planilha e guardar informações sobre nome e telefone
         workbook = openpyxl.load_workbook(file_path)
@@ -68,8 +74,10 @@ def enviar_mensagens(file_path, wait_time_whatsapp, wait_time_message, mensagem_
 
             # Verificar se o telefone está no formato correto do WhatsApp (55 + número)
             if len(Telefone) != 13:
-                logging.error(f"Número de telefone inválido para {Nome} ({Telefone})")
-                log_text.insert(tk.END, f"Número de telefone inválido para {Nome} ({Telefone})\n")
+                logging.error(f"Número de telefone inválido para {
+                              Nome} ({Telefone})")
+                log_text.insert(tk.END, f"Número de telefone inválido para {
+                                Nome} ({Telefone})\n")
                 logs["failure"].append((Nome, Telefone))
                 continue
 
@@ -77,14 +85,16 @@ def enviar_mensagens(file_path, wait_time_whatsapp, wait_time_message, mensagem_
             mensagem = mensagem_template.replace("{Nome}", Nome)
 
             # Criar link personalizado do WhatsApp
-            link_mensagem_whatsapp = f'https://web.whatsapp.com/send?phone={Telefone}&text={quote(mensagem)}'
+            link_mensagem_whatsapp = f'https://web.whatsapp.com/send?phone={
+                Telefone}&text={quote(mensagem)}'
             webbrowser.open(link_mensagem_whatsapp)
             time.sleep(wait_time_message)  # Ajuste o tempo conforme necessário
 
             try:
                 # Pressionar Enter para enviar a mensagem
                 pyautogui.press('enter')
-                time.sleep(6)  # Pausa para garantir que a mensagem seja enviada
+                # Pausa para garantir que a mensagem seja enviada
+                time.sleep(6)
 
                 # Fechar a aba atual
                 pyautogui.hotkey('ctrl', 'w')
@@ -92,11 +102,14 @@ def enviar_mensagens(file_path, wait_time_whatsapp, wait_time_message, mensagem_
 
                 # Registrar sucesso no log
                 logging.info(f'Mensagem enviada para {Nome} ({Telefone})')
-                log_text.insert(tk.END, f'Mensagem enviada para {Nome} ({Telefone})\n')
+                log_text.insert(tk.END, f'Mensagem enviada para {
+                                Nome} ({Telefone})\n')
                 logs["success"].append((Nome, Telefone))
             except Exception as e:
-                logging.error(f"Erro ao enviar mensagem para {Nome} ({Telefone}): {str(e)}")
-                log_text.insert(tk.END, f"Erro ao enviar mensagem para {Nome} ({Telefone}): {str(e)}\n")
+                logging.error(f"Erro ao enviar mensagem para {
+                              Nome} ({Telefone}): {str(e)}")
+                log_text.insert(tk.END, f"Erro ao enviar mensagem para {
+                                Nome} ({Telefone}): {str(e)}\n")
                 logs["failure"].append((Nome, Telefone))
 
             log_text.see(tk.END)
@@ -106,12 +119,14 @@ def enviar_mensagens(file_path, wait_time_whatsapp, wait_time_message, mensagem_
             progress_var.set((current_row / total_rows) * 100)
             progress_bar.update()
 
-        progress_var.set(100)  # Garantir que a barra de progresso chegue a 100% no final
+        # Garantir que a barra de progresso chegue a 100% no final
+        progress_var.set(100)
         progress_bar.update()
         messagebox.showinfo("Sucesso", "Mensagens enviadas com sucesso!")
     except Exception as e:
         logging.error(f"Erro ao enviar mensagens: {str(e)}")
         messagebox.showerror("Erro", f"Ocorreu um erro: {str(e)}")
+
 
 def iniciar_envio():
     file_path = filedialog.askopenfilename(
@@ -131,38 +146,50 @@ def iniciar_envio():
         return
 
     stop_event.clear()
-    thread = Thread(target=enviar_mensagens, args=(file_path, wait_time_whatsapp, wait_time_message, mensagem_template))
+    thread = Thread(target=enviar_mensagens, args=(
+        file_path, wait_time_whatsapp, wait_time_message, mensagem_template))
     thread.start()
+
 
 def parar_envio():
     stop_event.set()
-    messagebox.showinfo("Interrupção", "O envio das mensagens foi interrompido.")
+    messagebox.showinfo(
+        "Interrupção", "O envio das mensagens foi interrompido.")
+
 
 def salvar_logs():
-    save_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")])
+    save_path = filedialog.asksaveasfilename(
+        defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")])
     if not save_path:
         return
-    
+
     workbook = openpyxl.Workbook()
     sheet = workbook.active
     sheet.title = "Logs de Envio"
-    
+
     sheet.append(["Status", "Nome", "Telefone"])
     for status, entries in logs.items():
         for nome, telefone in entries:
             sheet.append([status, nome, telefone])
-    
+
     workbook.save(save_path)
     messagebox.showinfo("Sucesso", "Logs salvos com sucesso!")
 
+
 def show_info_whatsapp():
-    messagebox.showinfo("Informação", "Tempo de espera para carregar WhatsApp: O tempo (em segundos) necessário para que o WhatsApp Web carregue completamente antes de iniciar o envio das mensagens.")
+    messagebox.showinfo(
+        "Informação", "Tempo de espera para carregar WhatsApp: O tempo (em segundos) necessário para que o WhatsApp Web carregue completamente antes de iniciar o envio das mensagens.")
+
 
 def show_info_message():
-    messagebox.showinfo("Informação", "Tempo de espera entre mensagens: O tempo (em segundos) necessário entre o envio de cada mensagem para garantir que o WhatsApp Web processe e envie a mensagem corretamente.")
+    messagebox.showinfo(
+        "Informação", "Tempo de espera entre mensagens: O tempo (em segundos) necessário entre o envio de cada mensagem para garantir que o WhatsApp Web processe e envie a mensagem corretamente.")
+
 
 def show_info_mensagem():
-    messagebox.showinfo("Informação", "Mensagem: Insira a mensagem a ser enviada. Utilize {Nome} como um espaço reservado que será substituído pelo nome do destinatário.")
+    messagebox.showinfo(
+        "Informação", "Mensagem: Insira a mensagem a ser enviada. Utilize {Nome} como um espaço reservado que será substituído pelo nome do destinatário.")
+
 
 # Configurar a interface gráfica
 root = tk.Tk()
@@ -174,22 +201,26 @@ style = Style(theme="lumen")
 frame_tempo_espera = Frame(root, padding=10)
 frame_tempo_espera.pack(padx=20, pady=10, fill="x", anchor="center")
 
-Label(frame_tempo_espera, text="Tempo de espera para carregar WhatsApp:").grid(row=0, column=0, sticky="e")
+Label(frame_tempo_espera, text="Tempo de espera para carregar WhatsApp:").grid(
+    row=0, column=0, sticky="e")
 entry_wait_time_whatsapp = Entry(frame_tempo_espera, width=5)
 entry_wait_time_whatsapp.insert(0, "60")
 entry_wait_time_whatsapp.grid(row=0, column=1, padx=(5, 10))
 
 Label(frame_tempo_espera, text="segundos").grid(row=0, column=2, sticky="w")
-info_button_whatsapp = Button(frame_tempo_espera, text="ℹ️", command=show_info_whatsapp)
+info_button_whatsapp = Button(
+    frame_tempo_espera, text="ℹ️", command=show_info_whatsapp)
 info_button_whatsapp.grid(row=0, column=3, padx=5)
 
-Label(frame_tempo_espera, text="Tempo de espera entre mensagens:").grid(row=1, column=0, sticky="e")
+Label(frame_tempo_espera, text="Tempo de espera entre mensagens:").grid(
+    row=1, column=0, sticky="e")
 entry_wait_time_message = Entry(frame_tempo_espera, width=5)
 entry_wait_time_message.insert(0, "20")
 entry_wait_time_message.grid(row=1, column=1, padx=(5, 10))
 
 Label(frame_tempo_espera, text="segundos").grid(row=1, column=2, sticky="w")
-info_button_message = Button(frame_tempo_espera, text="ℹ️", command=show_info_message)
+info_button_message = Button(
+    frame_tempo_espera, text="ℹ️", command=show_info_message)
 info_button_message.grid(row=1, column=3, padx=5)
 
 # Seção de mensagem
@@ -198,10 +229,12 @@ frame_mensagem.pack(padx=20, pady=10, fill="x", anchor="center")
 
 Label(frame_mensagem, text="Mensagem:").grid(row=0, column=0, sticky="w")
 text_mensagem = scrolledtext.ScrolledText(frame_mensagem, height=5, width=40)
-text_mensagem.insert(tk.END, "Olá {Nome}, conto com seu voto! https://www.instagram.com/dadoseleitorais")
+text_mensagem.insert(
+    tk.END, "Olá {Nome}, conto com seu voto! https://www.instagram.com/dadoseleitorais")
 text_mensagem.grid(row=1, column=0, columnspan=3, pady=10, padx=(0, 5))
 
-info_button_mensagem = Button(frame_mensagem, text="ℹ️", command=show_info_mensagem)
+info_button_mensagem = Button(
+    frame_mensagem, text="ℹ️", command=show_info_mensagem)
 info_button_mensagem.grid(row=1, column=3, padx=5, sticky="n")
 
 
@@ -210,17 +243,20 @@ frame_controle = Frame(root, padding=10)
 frame_controle.pack(padx=20, pady=10, fill="x")
 
 # Primeira coluna de botões
-botao_baixar_modelo = Button(frame_controle, text="Baixar Modelo de Planilha", command=baixar_modelo)
+botao_baixar_modelo = Button(
+    frame_controle, text="Baixar Modelo de Planilha", command=baixar_modelo)
 botao_baixar_modelo.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 
-botao_abrir = Button(frame_controle, text="Carregar Planilha e Enviar Mensagens", command=iniciar_envio)
+botao_abrir = Button(
+    frame_controle, text="Carregar Planilha e Enviar Mensagens", command=iniciar_envio)
 botao_abrir.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
 
 # Segunda coluna de botões
 botao_parar = Button(frame_controle, text="Parar Envio", command=parar_envio)
 botao_parar.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
 
-botao_salvar_logs = Button(frame_controle, text="Salvar Logs", command=salvar_logs)
+botao_salvar_logs = Button(
+    frame_controle, text="Salvar Logs", command=salvar_logs)
 botao_salvar_logs.grid(row=1, column=1, padx=10, pady=10, sticky="ew")
 
 # Ajuste para expandir as colunas igualmente
@@ -233,7 +269,8 @@ frame_progresso = Frame(root, padding=10)
 frame_progresso.pack(padx=20, pady=10, fill="x")
 
 progress_var = tk.DoubleVar()
-progress_bar = Progressbar(frame_progresso, variable=progress_var, maximum=100, mode='determinate')
+progress_bar = Progressbar(
+    frame_progresso, variable=progress_var, maximum=100, mode='determinate')
 progress_bar.pack(fill="x", pady=10)
 
 # Campo de texto para logs
